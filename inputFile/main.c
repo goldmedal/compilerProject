2382be25047411c39cdb2d7f345926834de74135
Jax <jax@Jax-VM.(none)>
2014-06-13 00:59:39 +0800

    done v2

--- inputFile/main.c
+++ inputFile/main.c
@@ -1,19 +1,34 @@
-int x ;
+int x ; // aaaa
+int y ;
 int main ( ) {
-	int a ;  // aaa
-	int b ;
-	a = 0 ;
-	while ( ! ( a ) ) { 
-	x = a + b ;
-	break ;
-	b = 1 ;
+	int a [ 3 ] ;
+	a [ 2 ] = a [ 0 ] + a [ 1 ] ;
+	x = 1 ;
+	y = 20 ;
+	
+	x = y + 1 ;
+	
+	if ( x > 0 ) {
+	x = 100 ;
+	} 
+	else {
+	x = 5 ; 
 	}
-
-
+	
+	y = 1 + 5 ;
+	
+	while ( x > 5 )
+	{
+	y = x + 40 ;
+	x = 3 ;
+	}
+	
+	x = 1 * y ;
+	
+	return x + 3 ;
 }
 
-int y ;
-
 int doub ( int c , int d ) {
+	
 	return c * d ;
 }
